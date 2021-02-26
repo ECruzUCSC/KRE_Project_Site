5052b05bafd54e0d0c4f5db6a48ca24d8cf92d4b
ECrucUCSC <naehevrenaiv@gmail.com>
2021-02-25 21:35:44 -0800

    added hide/display of active guide category. fixed isolet icon.

--- kre_guide_hub.js
+++ kre_guide_hub.js
@@ -1,10 +1,23 @@
 function underlineFocus()
 {
-  console.log("Something");
 
   $(".guide-button").click(function (event) {
   		$(".guide-button").removeClass("guide-button-focus");
       $(this).addClass("guide-button-focus");
       //console.log($(this).css("text-decoration"))
+
+
+      $("#beginner-guides").css("display", "none");
+      $("#hero-guides").css("display", "none");
+      $("#misc-guides").css("display", "none");
+      if($(this).text() === "Beginner") {
+        $("#beginner-guides").css("display", "block");
+      }
+      if($(this).text() === "Hero") {
+        $("#hero-guides").css("display", "block");
+      }
+      if($(this).text() === "Misc.") {
+        $("#misc-guides").css("display", "block");
+      }
   });
 }
